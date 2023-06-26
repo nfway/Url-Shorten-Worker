bf351c61a9c9a573776e382c328c5c279d15c7c6
No F Way <80670048+nfway@users.noreply.github.com>
2023-06-26 20:14:18 +0800

    Delete webserver.ts

--- webserver.ts
+++
@@ -1,9 +0,0 @@
-import { serve } from "https://deno.land/std/http/server.ts";
-import staticFiles from "https://deno.land/x/static_files/mod.ts";
-
-const serveFiles = (req: Request) => staticFiles('./')({ 
-    request: req, 
-    respondWith: (r: Response) => r 
-})
-
-serve((req) => serveFiles(req), { addr: ':3000' });
